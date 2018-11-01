18d968083265104ef170f2eff9cc2b58159ccdeb
Akshay Bhatia <aksbhatia@lm-sjn-21020524.paypalcorp.com>
2018-11-01 10:26:28 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server');
+        }
+    })
+
+    next();
+
+})
+
+app.use((req, res, next) => {
+    res.render('maintainance.hbs');
+})
+
+app.use(express.static(__dirname + '/public'))
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Hey There! Welcome to the website',
+    });
+
+    // // res.send('<h1>Hello Express!</h1>');
+    // res.send({
+    //     name: 'Akshay',
+    //     likes: [
+    //         'biking',
+    //         'cities'
+    //     ]
+    // });
+});
+
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req,res) => {
+    res.send({
+        errorMessge: 'Error'
+    });
+});
+
+app.listen(3001, () => {
+    console.log('Server is up on port 3001');
+});
